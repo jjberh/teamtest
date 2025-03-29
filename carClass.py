26e2d1ad95362c19ab574d8e2e65da81e24d217e
Tamunoopubo <m.taribi18@gmail.com>
2025-03-28 22:05:53 -0400

    car

---
+++ carClass.py
@@ -0,0 +1,43 @@
+class Car:
+
+    def __init__(self, model = "Toyota Camry", hp = 151, engine = "V6", zeroSixty = 5.7):
+        self.model = model
+        self.hp = hp
+        self.engine = engine
+        self.zeroSixty = zeroSixty
+
+
+
+    def whatCar(self):
+        print(f"Stats:\n Model: {self.model}, HP: {self.hp}, Engine: {self.engine}, 0-60: {self.zeroSixty}")
+
+    def setModel(self, model):
+        self.model = model
+
+    def getModel(self):
+        return self.model
+
+    def setHP(self, hp):
+        self.hp = hp
+
+    def getHP(self):
+        return self.hp
+
+    def setEngine(self, engine):
+        self.engine = engine
+
+    def getEngine(self):
+        return self.engine
+
+    def setZeroSixty(self, zeroSixty):
+        self.zeroSixty = zeroSixty
+
+    def getZeroSixty(self):
+        return self.zeroSixty
+
+
+
+benz = Car()
+# benz.whatCar()
+benz.setModel("Mercedez AMG GLE 63s")
+print(benz.getModel())
